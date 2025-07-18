d33383bed92c52823107ff6b1d900b412dd8bfd4
Abhinaya <170817258+JulieRiverside@users.noreply.github.com>
2025-07-18 14:28:15 +0000

    Update render link

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -2,8 +2,8 @@
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center p-4">
+    <div className="fixed inset-0 z-50 flex justify-center items-center bg-white bg-opacity-80">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-gray-200"></div>
     </div>
-  )
+  );
 }
